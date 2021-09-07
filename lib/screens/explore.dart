5db42da757148efa1a789a88cd3be3a3a9ead28d
Ali Shams <alishams.s1380@gmail.com>
2021-09-07 14:56:17 +0430

    fix

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -1,19 +1,20 @@
 import 'dart:math';
 import 'dart:ui';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:line_icons/line_icon.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:timelines/timelines.dart';
 import 'package:treaget/components/indicator_tab.dart';
+import 'package:treaget/components/indicator_tab_circle.dart';
 import 'package:treaget/components/loading.dart';
 import 'package:treaget/components/explore/samplesExplore.dart';
 import 'package:treaget/services/explore_service.dart';
 
 // ignore: must_be_immutable
 class Example08 extends StatefulWidget {
- 
   @override
   _ViewPostScreenState createState() => _ViewPostScreenState();
 }
@@ -21,7 +22,7 @@ class Example08 extends StatefulWidget {
 class _ViewPostScreenState extends State<Example08>
     with AutomaticKeepAliveClientMixin<Example08> {
   List _products = [];
-  ScrollController _scrollController;
+  ScrollController _scrollController = new ScrollController();
   @override
   bool get wantKeepAlive => true;
   int _currentPage = 1;
@@ -38,21 +39,21 @@ class _ViewPostScreenState extends State<Example08>
   }
 
   Widget streamListView() {
-    return 
-        _products.length == 0 && _isLoading
-            ? loadingView()
-            : _products.length == 0
-                ? listIsEmpty()
-                :  Scaffold(body: RefreshIndicator(
+    return _products.length == 0 && _isLoading
+        ? loadingView()
+        : _products.length == 0
+            ? listIsEmpty()
+            : Scaffold(
+                backgroundColor: Colors.white,
+                body: RefreshIndicator(
                     child: Padding(
                       child: StaggeredGridView.countBuilder(
-                    
                         crossAxisCount: 4,
                         mainAxisSpacing: 4,
                         crossAxisSpacing: 4,
                         itemCount: _products.length,
                         // controller: widget._controller,
-                        // controller: _listScrollController,
+                        // controller: _scrollController,
                         itemBuilder: (context, index) {
                           return samplesExplore(index, _products[index]);
                         },
@@ -61,24 +62,28 @@ class _ViewPostScreenState extends State<Example08>
                       ),
                       padding: EdgeInsets.symmetric(horizontal: 10),
                     ),
-                    onRefresh: _handleRefresh),);
+                    onRefresh: _handleRefresh),
+              );
   }
 
   @override
   void initState() {
     super.initState();
     _getPost();
+    _scrollController.addListener(_scrollListener);
+  }
 
-    _scrollController.addListener(() {
-      // double maxScroll = _listScrollController.position.maxScrollExtent;
-      // double currentScroll = _listScrollController.position.pixels;
+  _scrollListener() {
+    if (_scrollController.hasClients) {
+      double maxScroll = _scrollController.position.maxScrollExtent;
+      double currentScroll = _scrollController.position.pixels;
 
-      // if (maxScroll - currentScroll <= 200) {
-      //   if (!_isLoading) {
-      //     _getPost(page: _currentPage + 1);
-      //   }
-      // }
-    });
+      if (maxScroll - currentScroll <= 200) {
+        if (!_isLoading) {
+          _getPost(page: _currentPage + 1);
+        }
+      }
+    }
   }
 
   _getPost({int page: 1, bool refresh: false}) async {
@@ -104,7 +109,7 @@ class _ViewPostScreenState extends State<Example08>
         controller: _scrollController,
         body: Container(
             child: DefaultTabController(
-                length: 2,
+                length: 3,
                 child: Scaffold(
                     backgroundColor: Colors.white,
                     appBar: AppBar(
@@ -113,21 +118,12 @@ class _ViewPostScreenState extends State<Example08>
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             TabBar(
-                              isScrollable: false,
-                              indicatorColor: Colors.transparent,
-                              indicatorSize: TabBarIndicatorSize.tab,
-                              unselectedLabelColor: Colors.grey[400],
-                              indicator: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.grey.withOpacity(0.2),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey.withOpacity(0.2),
-                                    blurRadius: 25,
-                                    offset: Offset(0, 15),
-                                  ),
-                                ],
-                              ),
+                              // isScrollable: true,
+                              indicatorColor: Colors.black,
+                              labelColor: Colors.black,
+                              unselectedLabelColor: Colors.grey[500],
+                              indicator: CircleTabIndicator(color: Colors.black, radius: 3),
+                              // indicator: CircleTabIndicator(color: Colors.green, radius: 4),
                               tabs: <Widget>[
                                 Tab(
                                   // icon: Icon(Icons.cloud_outlined),
@@ -137,90 +133,92 @@ class _ViewPostScreenState extends State<Example08>
                                   // icon: Icon(Icons.beach_access_sharp),
                                   child: Text("درخواست ها"),
                                 ),
+                                Tab(
+                                  // icon: Icon(Icons.beach_access_sharp),
+                                  child: Text("خدمات ها"),
+                                ),
                               ],
                             ),
                           ],
                         )),
                     body: TabBarView(children: [
-                      Container(child: streamListView(),),
+                      Container(
+                        child: streamListView(),
+                      ),
+                      Center(
+                        child: Text('It\'s sunny here'),
+                      ),
                       Center(
                         child: Text('It\'s sunny here'),
                       ),
                     ])))));
   }
-Widget topPage() {
-  return SliverToBoxAdapter(
-    child: Container(
-      child: Column(
-        children: [
-          AppBar(
-            elevation: 0,
-            title: Row(
-              textDirection: TextDirection.rtl,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Text("data")],
+
+  Widget topPage() {
+    return SliverToBoxAdapter(
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppBar(
+              elevation: 0,
+              title: Row(
+                textDirection: TextDirection.rtl,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [],
+              ),
             ),
-          ),
-           Text("data"),                 ],
+            Text(
+              "explore",
+              style: TextStyle(fontSize: 20),
+            ),
+            Form(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Container(
+                    padding: EdgeInsets.only(
+                        top: 20, bottom: 20, left: 20, right: 20),
+                    child: TextFormField(
+                      decoration: new InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey[100],
+                          icon: Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 10),
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(17)),
+                            child: Icon(
+                              LineIcons.horizontalSliders,
+                              color: Colors.white,
+                            ),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: new BorderSide(color: Colors.white),
+                            borderRadius: new BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          errorStyle: TextStyle(color: Colors.red),
+                          errorBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red)),
+                          focusedErrorBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red)),
+                          hintText: "search",
+                          hintStyle: TextStyle(fontSize: 15),
+                          contentPadding: EdgeInsets.only(
+                              top: 5, right: 10, bottom: 5, left: 20)),
+                    ),
+                  ))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-    ),
-  );
-}
-
-
-  // @override
-  // Widget buildnew(BuildContext context) {
-  //   return DefaultTabController(
-  //     initialIndex: 0,
-  //     length: 2,
-  //     child: Scaffold(
-  //       backgroundColor: Colors.white,
-  //       floatingActionButton: FloatingActionButton(
-  //         backgroundColor: Colors.grey[200],
-  //         foregroundColor: Colors.black,
-  //         onPressed: () {},
-  //         child: Icon(LineIcons.search),
-  //       ),
-  //       appBar: AppBar(
-  //         elevation: 1,
-  //         backgroundColor: Colors.white,
-  //         // title: const Text('TabBar Widget'),
-  //         flexibleSpace: Column(
-  //           mainAxisAlignment: MainAxisAlignment.end,
-  //           children: [
-  //             TabBar(
-  //               // isScrollable: true,
-  //               // indicatorColor: Colors.black,
-  //               unselectedLabelColor: Colors.grey,
-  //               indicator: MD2Indicator(
-  //                 indicatorSize: MD2IndicatorSize.normal,
-  //                 indicatorHeight: 2.0,
-  //                 indicatorColor:
-  //                     _isLoading == false ? Colors.black : Colors.transparent,
-  //               ),
-  //               tabs: <Widget>[
-  //                 Tab(
-  //                   // icon: Icon(Icons.cloud_outlined),
-  //                   child: Text("نمونه کار"),
-  //                 ),
-  //                 Tab(
-  //                   // icon: Icon(Icons.beach_access_sharp),
-  //                   child: Text("درخواست ها"),
-  //                 ),
-  //               ],
-  //             ),
-  //           ],
-  //         ),
-  //       ),
-  //       body: TabBarView(
-  //         children: <Widget>[
-  //           streamListView(),
-  //           Center(
-  //             child: Text('It\'s sunny here'),
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
+    );
+  }
 }
